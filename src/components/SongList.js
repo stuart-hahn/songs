ff399b0f537a962e736e0173dcaeeea3757bd665
Stuart Hahn <47613093+stuart-hahn@users.noreply.github.com>
2020-05-21 13:50:37 -0400

    wire up selectSong to dispatch by passing it through connect

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
+import { selectSong } from "../actions";
 
 class SongList extends React.Component {
   renderList() {
@@ -7,7 +8,12 @@ class SongList extends React.Component {
       return (
         <div className='item' key={song.title}>
           <div className='right floated content'>
-            <button className='ui button primary'>Select</button>
+            <button
+              className='ui button primary'
+              onClick={() => this.props.selectSong(song)}
+            >
+              Select
+            </button>
           </div>
           <div className='content'>{song.title}</div>
         </div>
@@ -16,7 +22,6 @@ class SongList extends React.Component {
   }
 
   render() {
-    console.log(this.props);
     return <div className='ui divided list'>{this.renderList()}</div>;
   }
 }
@@ -25,4 +30,4 @@ const mapStateToProps = (state) => {
   return { songs: state.songs };
 };
 
-export default connect(mapStateToProps)(SongList);
+export default connect(mapStateToProps, { selectSong })(SongList);
